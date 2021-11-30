90f6ab16b79a21d7cd54b2d220a9c0ecaf3312d0
Marcos Nolasco <marcosabn@gmail.com>
2021-11-30 15:01:09 -0300

    first urls

--- hctest/server/urls.py
+++ hctest/server/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from manager.views import EmpresaViewSet, FuncionarioViewSet
+from rest_framework import routers
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+router = routers.DefaultRouter()
+router.register('empresas', EmpresaViewSet, basename='Empresas')
+router.register('funcionarios', FuncionarioViewSet, basename='Funcionarios')
+
+urlpatterns = [path('admin/', admin.site.urls), path('', include(router))]
