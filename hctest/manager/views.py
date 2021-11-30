68252c308521ad50c302e4b78c4f280fae526ce1
Marcos Nolasco <marcosabn@gmail.com>
2021-11-30 14:55:40 -0300

    first viewsets

--- hctest/manager/views.py
+++ hctest/manager/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.db.models import query
+from rest_framework import serializers, viewsets
+from manager.models import Empresa, Funcionario
+from manager.serializers import EmpresaSerializer, FuncionarioSerializer
 
-# Create your views here.
+
+class EmpresaViewSet(viewsets.ModelViewSet):
+    """ Exibe todas as empresas """
+    queryset = Empresa.objects.all()
+    serializer_class = EmpresaSerializer
+
+
+class FuncionarioViewSet(viewsets.ModelViewSet):
+    """ Exibe todos os funcionarios """
+    queryset = Funcionario.objects.all()
+    serializer_class = FuncionarioSerializer
