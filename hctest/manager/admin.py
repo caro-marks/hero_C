3c74654bcd61d3a024dafc6ebc91d464a92fc5ca
Marcos Nolasco <marcosabn@gmail.com>
2021-11-30 14:47:53 -0300

    added to django admin site manager

--- hctest/manager/admin.py
+++ hctest/manager/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from manager.models import Empresa, Funcionario
 
-# Register your models here.
+
+class Empresas(admin.ModelAdmin):
+    list_display = ('id', 'nome_social', 'cnpj')
+    list_display_links = ('id', 'cnpj')
+    search_fields = ('cnpj', )
+
+
+admin.site.register(Empresas)
+
+
+class Funcionarios(admin.ModelAdmin):
+    list_display = ('id', 'primeiro_nome', 'ultimo_nome', 'cpf', 'trabalha_em')
+    list_display_links = ('id', 'cpf')
+    search_fields = ('cpf', )
+    list_per_page = 25
+
+
+admin.site.register(Funcionarios)
